c5b7b53b3883ba8b7ffe48e7e4fa0c6768ec4587
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-24 11:10:56 +0200

    Refactor clickable icon grid for navigation

--- pages/00_Accueil.py
+++ pages/00_Accueil.py
@@ -5,32 +5,29 @@ st.set_page_config(page_title="Accueil", page_icon="🏠")
 st.title("🐾 Club Canin – Accueil")
 st.write("Choisissez une section :")
 
-# Style pour agrandir les icônes
-icone_style = """
-<div style='font-size: 48px; text-align: center;'>
-    {icone}
-</div>
-<div style='text-align: center; font-size: 18px; margin-top: -10px;'>
-    {label}
-</div>
-"""
-
 # --- Grille d'icônes cliquables ---
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.page_link("pages/01_Membres.py", label=icone_style.format(icone="👥", label="Membres"), unsafe_allow_html=True)
+    st.page_link("pages/01_Membres.py", label="👥 Membres")
 
 with col2:
-    st.page_link("pages/02_Chiens.py", label=icone_style.format(icone="🐶", label="Chiens"), unsafe_allow_html=True)
+    st.page_link("pages/02_Chiens.py", label="🐶 Chiens")
 
 with col3:
-    st.page_link("pages/04_Cours.py", label=icone_style.format(icone="📘", label="Cours"), unsafe_allow_html=True)
+    st.page_link("pages/04_Cours.py", label="📘 Cours")
 
 col4, col5, col6 = st.columns(3)
 
 with col4:
-    st.page_link("pages/20_Cotisations.py", label=icone_style.format(icone="💰", label="Finances"), unsafe_allow_html=True)
+    st.page_link("pages/20_Cotisations.py", label="💰 Finances")
 
 with col5:
-    st.page_link("pages/33_presence_du_jour.py", label=icone_style.format(icone="👣", label
+    st.page_link("pages/33_presence_du_jour.py", label="👣 Présences")
+
+with col6:
+    st.page_link("pages/50_Inscription_En_Ligne.py", label="🌐 Public")
+
+st.markdown("---")
+
+st.page_link("pages/10_Parametres.py", label="⚙️ Technique")
