b1fbac1e3fa9812fd3523b5b4139843884f3ce83
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-24 10:39:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title="Club Canin", page_icon="🐾")
+
+st.title("🐾 Club Canin")
+st.write("Bienvenue dans votre application.")
+st.write("Utilisez le menu à gauche pour naviguer.")
