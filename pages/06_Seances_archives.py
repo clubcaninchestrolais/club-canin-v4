f55f67fa8f0bef31343aef6744571f9e2b46cf25
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-25 15:37:19 +0200

    Update 06_Seances_archives.py

--- pages/06_Seances_archives.py
+++ pages/06_Seances_archives.py
@@ -1,4 +1,9 @@
 import streamlit as st
+
+# --- SÉCURITÉ : accès réservé aux utilisateurs connectés ---
+if "connected" not in st.session_state or not st.session_state["connected"]:
+    st.switch_page("pages/login.py")
+
 from supabase import create_client
 
 st.set_page_config(page_title="Séances archivées", page_icon="📚")
