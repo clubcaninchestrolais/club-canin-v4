50ad047503976c1d6a72167691a23f507c4dcbd6
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-25 13:27:42 +0200

    Restore and format 00_Accueil.py content

--- pages/00_Accueil.py
+++ pages/00_Accueil.py
@@ -88,7 +88,7 @@ with colA:
     bloc("50_Inscription_En_Ligne", "🌐", "Préinscription publique")
 
 with colB:
-    bloc("60_Validation_preinscriptions", "📝", "Validation préinscriptions")
+    bloc("60_Validation_préinscriptions", "📝", "Validation préinscriptions")
 
 with colC:
     bloc("33_presence_du_jour", "👣", "Présences du jour")
