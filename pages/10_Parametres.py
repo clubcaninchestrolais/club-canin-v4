ee5d8318ddfa09bdaca7b811307c2285fddb9298
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-25 17:33:02 +0200

    Update 10_Parametres.py

--- pages/10_Parametres.py
+++ pages/10_Parametres.py
@@ -5,8 +5,16 @@ if "connected" not in st.session_state or not st.session_state["connected"]:
     st.switch_page("pages/login.py")
 
 from supabase_rest import supabase
+from menu import hide_streamlit_menu, menu_lateral   # <-- AJOUT
 
 st.set_page_config(page_title="Paramètres du club", page_icon="⚙️")
+
+# --- MASQUER LE MENU AUTOMATIQUE ---
+hide_streamlit_menu()   # <-- AJOUT
+
+# --- AFFICHER LE MENU PERSONNALISÉ ---
+menu_lateral()          # <-- AJOUT
+
 st.title("⚙️ Paramètres du club")
 
 # Charger les paramètres (une seule ligne)
