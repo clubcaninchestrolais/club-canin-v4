49cfa7168980ccdae5ead44687008321f54a0cdb
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-25 09:51:57 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -30,7 +30,7 @@ def menu_lateral():
     st.sidebar.page_link("pages/01_Membres.py", label="👥 Membres")
     st.sidebar.page_link("pages/02_Chiens.py", label="🐶 Chiens")
     st.sidebar.page_link("pages/04_Cours.py", label="📘 Cours")
-    st.sidebar.page_link("pages/20_Cotisations.py", label="💰 Finances")
+    st.sidebar.page_link("pages/20_Cotisations.py", label="💰 Cotisations")
     st.sidebar.page_link("pages/33_presence_du_jour.py", label="👣 Présences")
     st.sidebar.page_link("pages/50_Inscription_En_Ligne.py", label="🌐 Public")
     st.sidebar.page_link("pages/10_Parametres.py", label="⚙️ Technique")
