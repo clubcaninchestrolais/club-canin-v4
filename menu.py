728933e179cf71c45cb97d1f0bcae27014187f51
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-26 16:21:41 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -56,6 +56,8 @@ def menu_lateral():
     st.sidebar.markdown("### 🔄 Flux")
     st.sidebar.page_link("pages/50_Inscription_En_Ligne.py", label="🌐 Préinscription publique")
     st.sidebar.page_link("pages/60_Validation_preinscription.py", label="📝 Validation préinscription")
+    st.sidebar.page_link("pages/61_Liste_preinscriptions_exterieurs.py", label="📋 listing Préinscriptions extérieures")
+
 
     st.sidebar.markdown("### 🏛️ Organisations")
     st.sidebar.page_link("pages/organisations.py", label="🏛️ Organisations")
