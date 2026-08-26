7ab00cbdaec44a20053fdc0467cdf8d300c83ff7
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-26 09:31:09 +0200

    Update 70_Validation_presences.py

--- pages/70_Validation_presences.py
+++ pages/70_Validation_presences.py
@@ -1,13 +1,18 @@
 import streamlit as st
+from menu import hide_streamlit_menu, menu_lateral
+from supabase_rest import supabase
+from datetime import datetime
 
 # --- SÉCURITÉ : accès réservé aux utilisateurs connectés ---
 if "connected" not in st.session_state or not st.session_state["connected"]:
     st.switch_page("pages/login.py")
 
-from supabase_rest import supabase
-from datetime import datetime
-
 st.set_page_config(page_title="Validation des présences", page_icon="🟢")
+
+# --- MENU PERSONNALISÉ ---
+hide_streamlit_menu()
+menu_lateral()
+
 st.title("🟢 Validation des présences")
 
 # ---------------------------------------------------------
