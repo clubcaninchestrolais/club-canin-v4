d7faa2c52bf7199784a12709636591ac61983508
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-29 12:19:13 +0200

    Create temporaire.py

---
+++ pages/temporaire.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from supabase import create_client, Client
+import datetime
+
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+supabase: Client = create_client(url, key)
+
+aujourdhui = datetime.date.today().isoformat()
+
+seances = (
+    supabase.table("cours_seances")
+    .select("*")
+    .eq("date_seance", aujourdhui)
+    .execute()
+    .data
+)
+
+st.write("Cours_id réels :")
+for s in seances:
+    st.write(repr(s["cours_id"]))
