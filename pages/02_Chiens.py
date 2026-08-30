0fa094ba599c6087f79e33d7236f208e2d64b3e0
clubcaninchestrolais <jmarch6840@gmail.com>
2026-08-30 07:08:34 +0200

    Refactor chien page with security and imports

--- pages/02_Chiens.py
+++ pages/02_Chiens.py
@@ -1,12 +1,10 @@
 import streamlit as st
+from securite import securite_user
+securite_user()
 
-# --- SÉCURITÉ : accès réservé aux utilisateurs connectés ---
-if "connected" not in st.session_state or not st.session_state["connected"]:
-    st.switch_page("pages/login.py")
-
-import datetime
 from supabase_rest import supabase
 from menu import hide_streamlit_menu, menu_lateral
+
 
 # --- CONFIGURATION DE LA PAGE ---
 st.set_page_config(page_title="Chiens", page_icon="🐶", layout="wide")
