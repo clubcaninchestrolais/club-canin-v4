b31a2cdf660f6a0b2057aea0e5454ce6fe07c707
clubcaninchestrolais <jmarch6840@gmail.com>
2026-09-02 08:50:58 +0200

    Update securite.py

--- securite.py
+++ securite.py
@@ -12,3 +12,16 @@ def securite_admin():
     if st.session_state.get("role") != "admin":
         st.error("Accès réservé à l'administration.")
         st.stop()
+def securite_globale():
+    """Sécurité globale : protège toutes les pages automatiquement."""
+    
+    # Garantit que les clés existent
+    if "connected" not in st.session_state:
+        st.session_state["connected"] = False
+
+    if "role" not in st.session_state:
+        st.session_state["role"] = "user"
+
+    # Protection connexion
+    if not st.session_state["connected"]:
+        st.switch_page("pages/login.py")
