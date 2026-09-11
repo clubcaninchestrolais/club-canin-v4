f6a960164b40c5bb8e1d1323ab0a4297bee8aebd
clubcaninchestrolais <jmarch6840@gmail.com>
2026-09-11 10:21:16 +0200

    Rename '15_Magasin_Produits' to 'Magasin_Produits'

--- pages/120_Backup.py
+++ pages/120_Backup.py
@@ -39,7 +39,7 @@ tables = [
     "depenses",
     "finances",
     "clotures",
-    "15_Magasin_Produits",
+    "Magasin_Produits",
     "Magasin_Achats",
     "Magasin_Ventes",
     "Magasin_Stats",
