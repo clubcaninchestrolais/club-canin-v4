47b5188e0d6a59021af7b17b12076d0228558add
clubcaninchestrolais <jmarch6840@gmail.com>
2026-09-13 11:18:53 +0200

    Update accueil.py

--- accueil.py
+++ accueil.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import time
 
-# --- PAGE CONFIG ---
 st.set_page_config(page_title="Club Canin Chestrolais", page_icon="🐾", layout="centered")
 
-# --- CSS ANIMATION ---
 fade_css = """
 <style>
 @keyframes fadeIn {
@@ -18,14 +16,12 @@ fade_css = """
 """
 st.markdown(fade_css, unsafe_allow_html=True)
 
-# --- SPLASH SCREEN ---
 splash = st.empty()
 
 with splash.container():
     st.markdown(
         """
         <div class="fade-in" style="text-align:center; margin-top:120px;">
-            <img src="/logo.png" width="300">
             <h1 style="color:#003366; font-size:36px; margin-top:20px;">
                 Club Canin Chestrolais
             </h1>
@@ -37,14 +33,9 @@ with splash.container():
         unsafe_allow_html=True
     )
 
-# --- TEMPS D'AFFICHAGE DU LOGO ---
 time.sleep(2)
 
-# --- AFFICHAGE DU LOGIN ---
 splash.empty()
 
 st.title("🔐 Connexion")
 st.write("Veuillez vous connecter pour accéder au portail interne.")
-
-# Ici tu mets ton module de login habituel :
-# login_form()
