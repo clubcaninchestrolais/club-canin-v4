e07be387dea64d0cec764111566d97a347bc6d97
clubcaninchestrolais <jmarch6840@gmail.com>
2026-09-13 10:15:37 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -42,7 +42,8 @@ def menu_lateral():
     st.sidebar.markdown(
         """
         <div style="text-align: center; margin-bottom: 15px;">
-            <img src="logoJMH.jpg" style="width: 120px; border-radius: 10px;">
+            <img src="images/logoJMH.jpg" style="width: 120px; border-radius: 10px;">
+
         </div>
         """,
         unsafe_allow_html=True
